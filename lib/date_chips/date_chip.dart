8e0d19744ef20a10b38be46c9c78f7792f428afc
Prabhanu <prabhanu96@gmail.com>
2021-05-15 10:47:41 +0530

    DateChip widget change color option

--- lib/date_chips/date_chip.dart
+++ lib/date_chips/date_chip.dart
@@ -1,25 +1,43 @@
 import 'package:chat_bubbles/algo/algo.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 ///[DateChip] use to show the date breakers on the chat view
+///[date] parameter is required
+///[color] parameter is optional default color code `8AD3D5`
 ///
 ///
-class DateChip extends StatelessWidget{
+class DateChip extends StatelessWidget {
   final DateTime date;
+  final Color color;
+
   ///
   ///
   ///
   const DateChip({
     Key? key,
     required this.date,
+    this.color = const Color(0x558AD3D5),
   }) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(
-        Algo.dateChipText(date),
+    return Padding(
+      padding: const EdgeInsets.only(
+        top: 7,
+        bottom: 7,
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(6)),
+          color: color,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: Text(
+            Algo.dateChipText(date),
+          ),
+        ),
       ),
     );
   }
-
 }
