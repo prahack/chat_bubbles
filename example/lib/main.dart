5690bbf701b039b7794cf9719edfb32b7799c4e1
Prabhanu <prabhanu96@gmail.com>
2021-05-15 10:48:01 +0530

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -27,6 +27,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    final now = new DateTime.now();
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -36,13 +37,10 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              DateChip(
-                date: new DateTime.now(),
-              ),
               BubbleNormal(
                 text: 'bubble normal with tail',
                 isSender: false,
-                color: Color(0xAF6AD0F5),
+                color: Color(0xFF1B97F3),
                 tail: true,
                 textStyle: TextStyle(
                   fontSize: 20,
@@ -52,14 +50,17 @@ class _MyHomePageState extends State<MyHomePage> {
               BubbleNormal(
                 text: 'bubble normal with tail',
                 isSender: true,
-                color: Color(0xFFE2FFC7),
+                color: Color(0xFFE8E8EE),
                 tail: true,
                 sent: true,
+              ),
+              DateChip(
+                date: new DateTime(now.year, now.month, now.day - 2),
               ),
               BubbleNormal(
                 text: 'bubble normal without tail',
                 isSender: false,
-                color: Color(0xAF6AD0F5),
+                color: Color(0xFF1B97F3),
                 tail: false,
                 textStyle: TextStyle(
                   fontSize: 20,
@@ -68,7 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               BubbleNormal(
                 text: 'bubble normal without tail',
-                color: Color(0xFFE2FFC7),
+                color: Color(0xFFE8E8EE),
                 tail: false,
                 sent: true,
                 seen: true,
@@ -77,62 +78,68 @@ class _MyHomePageState extends State<MyHomePage> {
               BubbleSpecialOne(
                 text: 'bubble special one with tail',
                 isSender: false,
-                color: Color(0xAF6AD0F5),
+                color: Color(0xFF1B97F3),
                 textStyle: TextStyle(
                   fontSize: 20,
-                  color: Colors.teal,
+                  color: Colors.white,
                 ),
+              ),
+              DateChip(
+                date: new DateTime(now.year, now.month, now.day - 1),
               ),
               BubbleSpecialOne(
                 text: 'bubble special one with tail',
-                color: Color(0xFFE2FFC7),
+                color: Color(0xFFE8E8EE),
                 seen: true,
               ),
               BubbleSpecialOne(
                 text: 'bubble special one without tail',
                 isSender: false,
                 tail: false,
-                color: Color(0xAF6AD0F5),
+                color: Color(0xFF1B97F3),
                 textStyle: TextStyle(
                   fontSize: 20,
-                  color: Colors.teal,
+                  color: Colors.black,
                 ),
               ),
               BubbleSpecialOne(
                 text: 'bubble special one without tail',
                 tail: false,
-                color: Color(0xFFE2FFC7),
+                color: Color(0xFFE8E8EE),
                 sent: true,
               ),
               BubbleSpecialTwo(
                 text: 'bubble special tow with tail',
                 isSender: false,
-                color: Color(0xAF6AD0F5),
+                color: Color(0xFF1B97F3),
                 textStyle: TextStyle(
                   fontSize: 20,
-                  color: Colors.red,
+                  color: Colors.black,
                 ),
+              ),
+              DateChip(
+                date: now,
               ),
               BubbleSpecialTwo(
                 text: 'bubble special tow with tail',
                 isSender: true,
-                color: Color(0xFFE2FFC7),
+                color: Color(0xFFE8E8EE),
                 sent: true,
               ),
               BubbleSpecialTwo(
                 text: 'bubble special tow without tail',
                 isSender: false,
                 tail: false,
-                color: Color(0xAF6AD0F5),
+                color: Color(0xFF1B97F3),
                 textStyle: TextStyle(
                   fontSize: 20,
-                  color: Colors.red,
+                  color: Colors.black,
                 ),
               ),
               BubbleSpecialTwo(
                 text: 'bubble special tow without tail',
                 tail: false,
-                color: Color(0xFFE2FFC7),
+                color: Color(0xFFE8E8EE),
                 delivered: true,
               ),
             ],
